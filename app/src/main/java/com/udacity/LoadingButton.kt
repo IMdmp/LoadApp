a7dce9d89aca8e98fe6610cf0855975431a24f56
Dominic <pagtalunan.d@gmail.com>
2020-12-26 04:49:46 +0800

    first ever working customview- canvas on text!

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -2,15 +2,24 @@ package com.udacity
 
 import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Paint.ANTI_ALIAS_FLAG
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.content.withStyledAttributes
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : AppCompatButton(context, attrs, defStyleAttr) {
+        context: Context, val attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private val backgroundColor: Int = ResourcesCompat.getColor(resources, R.color.purple, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.darkBlue, null)
+
     private var widthSize = 0
     private var heightSize = 0
 
@@ -20,24 +29,59 @@ class LoadingButton @JvmOverloads constructor(
 
     }
 
+    private var fontSize = 0f
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        textSize = fontSize
+    }
+
 
     init {
+        context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
+            fontSize = getDimension(R.styleable.LoadingButton_textSize, 0f)
+            Log.d("TEST", "fontsize : $fontSize")
+
+        }
+        paint.textSize = fontSize
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+
 
     }
 
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-
+//        canvas.drawBitmap(extraBitmap,0f,0f,null)
+        canvas.drawColor(backgroundColor)
+        canvas.drawText("sample text", 40f, 40f, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
         val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
+                MeasureSpec.getSize(w),
+                heightMeasureSpec,
+                0
         )
         widthSize = w
         heightSize = h
